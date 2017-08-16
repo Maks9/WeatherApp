6c9bbdaf8d7934090b1c5c1cc6382007e9ec442b
Администратор <makspman@gmail.com>
2017-08-16 23:11:25 +0300

    q

--- server/src/main/java/com/sky/Application.java
+++ server/src/main/java/com/sky/Application.java
@@ -1,10 +1,25 @@
 package com.sky;
 
-import java.util.ArrayList;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by fmandryka on 20.07.2017.
  */
+
+@RestController
+@EnableAutoConfiguration
 public class Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
